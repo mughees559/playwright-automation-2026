8819da52d14c26e3fae60a64dd1a4db925ac9eac
mughees559 <m.mughees559@gmail.com>
2026-05-29 05:47:11 +0500

    update the base url

--- config/environment.config.ts
+++ config/environment.config.ts
@@ -2,6 +2,6 @@ import * as dotenv from 'dotenv';
 dotenv.config(); // Load .env
 
 export const ENV = {
-  APP_URL: process.env.APP_URL || 'https://opensource-demo.orangehrmlive.com',
+  APP_URL: process.env.APP_URL || 'https://automationexercise.com/',
   
 };
